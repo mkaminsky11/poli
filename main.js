f04405e335d94f40d805499d6db01bcbfcaefcba
mkaminsky11 <mkaminsky11@gmail.com>
2017-02-01 17:18:46 -0500

    move sidebar

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 1500, height: 1000})
+  win = new BrowserWindow({width: 2000, height: 1000})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
